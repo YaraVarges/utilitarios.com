d1e24e4b852a5cce8e4730075e52d81eabc3d43a
Yara Varges Nunes <62305551+YaraVarges@users.noreply.github.com>
2024-04-06 20:39:55 -0300

    Update responsive

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 document.getElementById('open_btn').addEventListener('click', function () {
     document.getElementById('sidebar').classList.toggle('open-sidebar');
 });
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
