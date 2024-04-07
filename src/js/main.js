b051f0e08531531f5785d9e2a0c2eaa5dbc83cb5
Yara Varges Nunes <62305551+YaraVarges@users.noreply.github.com>
2024-04-07 17:35:32 -0300

    Update pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-document.getElementById('open_btn').addEventListener('click', function () {
-    document.getElementById('sidebar').classList.toggle('open-sidebar');
-});
-
 function menuOnClick() {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
